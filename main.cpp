5160d059c8f745f41eeda8c245bc007474c75ca9
pasupuletiravikumar <149005025+pasupuletiravikumar@users.noreply.github.com>
2025-03-25 22:17:51 +0530

    commit message:testing the functionality of secure interprocess communication

--- main.cpp
+++ main.cpp
@@ -1,23 +1,45 @@
+#include <iostream>
 #include "include/interprocess.h"
+#include "include/security_layer.h"
+#include "include/management.h"
+using namespace std;
 
 int main() {
-    printf("Creating shared memory\n");
+    string username, password;
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+
+    if (!authenticate(username, password)) {
+        log_message("Failed login attempt: " + username);
+        cout << "Authentication Failed!" << endl;
+        return 0;
+    }
+
+    log_message("Login successful: " + username);
     create_shm();
 
-    string data_to_write = "Yo! This is for you.\n";
-    printf("Writing to shared memory: %s", data_to_write.c_str());
-    write_to_shm(data_to_write);
-    
-    string message_to_send = data_to_write;
-    printf("Sending message\n");
-    send_msg(message_to_send);
-    
-    string message_received = recive_msg();
-    printf("Message received from queue\n");
-    
-    string data_read = read_from_shm();
-    printf("Data read from shared memory: %s\n", data_read.c_str());
+    string message;
+    cout << "Enter your message: ";
+    cin.ignore();
+    getline(cin, message);
 
+    string encrypted_message = encrypt(message);
+
+    write_to_shm(encrypted_message);
+    string shm_message = read_from_shm();
+    string decrypted_shm_message = decrypt(shm_message);
+
+    cout << "\nMessage from Shared Memory (Decrypted): " << decrypted_shm_message << endl;
+    log_message("Message read from shared memory and decrypted successfully.");
+
+    send_msg(encrypted_message);
+    string received_encrypted_msg = recive_msg();
+    string decrypted_queue_message = decrypt(received_encrypted_msg);
+
+    cout << "Message from Message Queue (Decrypted): " << decrypted_queue_message << endl;
+    log_message("Message sent, received, and decrypted successfully using message queue.");
 
     return 0;
 }
